0d5bcf3220026e5b7243e82acad60a36e7d0058f
Axel Bagenal <axel.bagenal@gmail.com>
2022-11-28 15:10:42 +0100

    Integrate User with Devise signin/signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # CONFIRM WITH TA
+
+  config.action_mailer.default_url_options = { host: 'https://bunker-and-breakfast.herokuapp.com/' }
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
