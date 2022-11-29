5f81552daa4078522d3e5c15a8c8582c3721b6fb
JeanBaptiste Allemandou <jeanbaptiste.allemandou@gmail.com>
2022-11-29 18:01:24 +0100

    manage booking and bunker page

--- app/controllers/bunkers_controller.rb
+++ app/controllers/bunkers_controller.rb
@@ -37,10 +37,20 @@ class BunkersController < ApplicationController
     @mybunkers = policy_scope(Bunker)
     authorize @mybunkers
     @mybunkers = Bunker.where(user_id: current_user.id)
-
+    @totalcash = total_cash_accepted(@mybunkers)
   end
 
   private
+
+  def total_cash_accepted(bunkers)
+    sum = 0
+    bunkers.each do |b|
+      b.bookings.each do |bo|
+        sum += bo.total_price
+      end
+    end
+    return sum
+  end
 
   def bunker_params
     params.require(:bunker).permit(:name, :description, :price, :location, :max_capacity)
