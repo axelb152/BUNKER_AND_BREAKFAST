d7e6bab1f36917c31ce44349add417716de4a772
Axel Bagenal <axel.bagenal@gmail.com>
2022-11-30 12:46:25 +0100

    change root to bunkers index page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # get 'bunkers/update'
   # get 'bunkers/destroy'
   devise_for :users
-  root to: "pages#home"
+  root to: "bunkers#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get 'bunkers/manage', to: 'bunkers#manage'
